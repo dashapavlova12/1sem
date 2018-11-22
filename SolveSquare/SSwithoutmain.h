3053a1f02651bf5a040767161c8d86e05569f3f9
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2018-11-23 02:28:14 +0300

    my_first_header

---
+++ SolveSquare/SSwithoutmain.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+#include <float.h>
+
+const int INF_ROOTS = 100;
+
+int SolveLinear (double b, double c, double * x1);
+
+int SolveSquare (double a,double b,double c, double * x1, double * x2);
