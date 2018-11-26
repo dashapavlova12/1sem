6686407181dea0023e98207bc0f4bebecc42011a
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2018-11-26 17:18:02 +0300

    Create assembler.cpp

---
+++ Soft_Processor/ASM/assembler.cpp
@@ -0,0 +1,119 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+enum errors
+{
+	MEMORY_ERROR = -10,
+	POINTER_ERROR,
+};
+
+int LABELS_COUNT = 32;
+
+int INITIAL_LINE = 128;
+ 
+int checkcom (char * line, FILE * bytecod, FILE * commands, int * labels);
+
+int mem_labels(FILE * commands, int * labels);
+
+int initialize_array(int * labels);
+ 
+int main ()
+{
+	int return_check = 0;
+    FILE * commands = fopen ("commands.txt", "r");
+    FILE * bytecod = fopen ("bytecod.txt", "w");
+    if(commands == NULL && bytecod == NULL)
+    {
+    	printf("Error opening files. Check the data.");
+    	return POINTER_ERROR;
+	}
+	
+    char * line = (char *)calloc(INITIAL_LINE, sizeof(char));
+    int * labels = (int *) calloc(LABELS_COUNT, sizeof(int));
+    if (line == NULL && labels == NULL)
+    {
+    	printf("Error allocating memory.");
+    	return MEMORY_ERROR;
+	}
+	
+    return_check = mem_labels(commands, labels); 
+    if (return_check == -1)
+    {
+    	printf("Error. Check the data.");
+    	return POINTER_ERROR;
+	}
+	
+    while (fscanf (commands, "%s", line) != EOF)
+    {
+        return_check = checkcom (line, bytecod, commands, labels);
+        if (return_check == -1)
+        {
+        	printf("Error. Check the data.");
+        	return POINTER_ERROR;
+    	}
+    }
+    return 0;
+}
+ 
+int mem_labels(FILE * commands, int * labels)
+{
+    if ( commands == NULL || labels == NULL )
+    {
+        return -1;
+    }
+    
+    int check_return = 0;
+    
+    check_return = initialize_array(labels);
+    if (check_return == -1)
+    {
+    	return -1;
+	}
+	
+    char curr_command[99];
+    int current_index = 0;
+    int label = 0;
+ 	
+    while(fscanf(commands, "%s", curr_command) != EOF)
+    {
+    	
+        if (strcmp(curr_command, ":") == 0)
+        {
+            fscanf(commands, "%d", &label);
+            labels[label] = current_index;
+        }
+        else
+        {
+            current_index += 1;
+        }
+        
+    }
+    fseek(commands, 0, SEEK_SET);
+    return 0;
+}
+ 
+int initialize_array(int * labels)
+{
+    if (labels == NULL)
+    {
+        return -1;
+    }
+    for (int i = 0; i < LABELS_COUNT; i++)
+        labels[i] = -1;
+    return 0;
+}
+ 
+int checkcom (char * line, FILE * bytecod, FILE * commands, int * labels)
+{
+	int arg = 0;
+    #define DEF_CMD(name, num, CODE_ASM, CODE_CPU)      \
+    if (strcmp(line, name) == 0)                       \
+    {                                                   \
+        CODE_ASM            \
+		                            \
+    }
+ 
+    #include "commands.h"
+    
+}
