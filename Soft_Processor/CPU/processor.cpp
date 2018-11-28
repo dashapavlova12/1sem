6213fb0ba96d704124091084e1213910fb8cd40c
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2018-11-28 23:07:45 +0300

    Update processor.cpp

--- Soft_Processor/CPU/processor.cpp
+++ Soft_Processor/CPU/processor.cpp
@@ -6,13 +6,16 @@
 enum errors 
 {
 	MEMORY_ERROR = -10,
-	POINTER_ERROR,
-		
+	POINTER_ERROR,	
 };
 
+int END = 100500;
+
 int MEMORY_SIZE = 128;
+
 int LABELS_COUNT = 32;
- 
+
+int COMMAND_INIT = 128;
  
 struct CPU
 {
@@ -36,8 +39,13 @@ int ExecuteCom (int com, struct CPU * MY_CPU, double * commands, int * curpos);
 
 int free_processor (double * commands, FILE * bytecod, struct CPU * MY_CPU);
  
-int main ()
+int main (int argc, char * argv[])
 {
+    if (argc != 2)
+	{
+		printf ("Error!!! 1 file expected");
+		return -1;
+	}
     struct CPU MY_CPU;
     int check_return = 0;
 
@@ -48,7 +56,7 @@ int main ()
     	return POINTER_ERROR;
 	}
 
-    FILE * bytecod = fopen ("bytecod.txt", "r");
+    argv[1] = fopen ("bytecod.txt", "r");
     if (bytecod == NULL)
     {
     	printf("File not found. Check the data.");
@@ -100,12 +108,12 @@ int do_programm(struct CPU * MY_CPU, double * commands)
 	
     int ret = 0;
     int curpos = 0;
-    while( ret != 100500 )
+    while( ret != END )
     {
         ret = ExecuteCom( commands[curpos], MY_CPU, commands, &curpos );
         if (ret == -1)
         {
-        	return ret;
+             return ret;
 		}
     }
     return 0;
@@ -115,15 +123,15 @@ int initialize_CPU(struct CPU * MY_CPU)
 {
     if (MY_CPU == NULL)
     {
-    	return POINTER_ERROR;
-	}
+     	return POINTER_ERROR;
+    }
 
     MY_CPU -> memory = (double *) calloc (MEMORY_SIZE, sizeof(double));
     
     if (MY_CPU -> memory == NULL)
     {
     	return MEMORY_ERROR;
-	}
+    }
 	
     MY_CPU -> ax = 0;
     MY_CPU -> bx = 0;
@@ -158,12 +166,12 @@ int mem_bytecod(FILE * bytecod, double * commands)
         return POINTER_ERROR;
     }
 
-    char curr_command[99];
+    double curr_command[COMMAND_INIT] = " ";
     int current_index = 0;
 
-    while(fscanf(bytecod, "%s", curr_command) != EOF)
+    while(fscanf(bytecod, "%lf", curr_command) != EOF)
     {
-        commands[current_index] = atof(curr_command);
+        commands[current_index] = curr_command;
         current_index += 1;
     }
     fseek(bytecod, 0, SEEK_SET);
@@ -174,20 +182,21 @@ int ExecuteCom (int com, struct CPU * MY_CPU, double * commands, int * curpos)
 {
 	if (com <= 0 || MY_CPU == NULL || commands == NULL || curpos == NULL)
 	{
-		return -1;
+	    return -1;
 	}
 	else
     {
 		switch (com)
     	{
-    	#define DEF_CMD(name, num, CODE_ASM, CODE_CPU)      \
-        	case(num):                                       \
-        	{                                               \
-            	CODE_CPU                                    \
-        	}
-    	#include "commands.h"
+   			 #define DEF_CMD(name, num, CODE_ASM, CODE_CPU)  \
+    			case(num):                                   \
+        		{                                            \
+             		CODE_CPU                                 \
+        		}
+    		 #include "commands.h"
     	}
-	}
+	return -1;
+    }
 }
 
 int free_processor (double * commands, FILE * bytecod, struct CPU * MY_CPU)
