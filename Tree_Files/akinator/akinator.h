b8996e7de3c88cf92eae750257bb3700ecb651b2
dashapavlova12 <43764359+dashapavlova12@users.noreply.github.com>
2018-12-04 22:47:38 +0300

    Update akinator.h

--- Tree_Files/akinator/akinator.h
+++ Tree_Files/akinator/akinator.h
@@ -5,3 +5,13 @@ node_t * read_database (FILE * database, node_t * parent);
 int fsize(FILE * database);
 
 int printf_file (node_t * node, FILE * database);
+
+int get_character (node_t * parent);
+
+int do_play (node_t * node);
+
+int questions (node_t * node);
+
+int graphviz (node_t * root);
+
+int print_dot(FILE * file, node_t * node);
